73344f7a33a05ab4f906f2cc128df7fe4f8398c6
vijay kumar <vk77093@gmail.com>
2021-04-19 10:16:37 +0530

    navbar function added

--- JavaScriptProject/4.NavbarToggle/app.js
+++ JavaScriptProject/4.NavbarToggle/app.js
@@ -3,3 +3,22 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+let togglesButton = document.querySelector('.nav-toggle');
+let linksCheck = document.querySelector('.links');
+
+// togglesButton.addEventListener('click',()=>{
+//     //console.log(linksCheck.classList);
+//     //for checking whether the class is their or not
+//     //console.log(linksCheck.classList.contains('show-links'));
+//     let trueOrNOT=linksCheck.classList.contains('show-links');
+//     if(trueOrNOT){
+//         linksCheck.classList.remove('show-links');
+//     }else{
+//         linksCheck.classList.add('show-links');
+//     }
+// })
+
+//with the help of toggle function
+togglesButton.addEventListener('click',()=>{
+    linksCheck.classList.toggle('show-links');
+})
